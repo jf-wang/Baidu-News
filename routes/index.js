04ab8eb4fafad9dc167cbaf1ed196337d0939c57
jf-wang <781186429@qq.com>
2017-04-19 10:34:05 +0800

    first demo

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+router.use(express.static(path.join(__dirname, 'public')));
+var mysql=require('mysql');
+//连接数据库
+var conn=mysql.createConnection({
+  host    :'localhost',
+      port  :3306,
+      user  :'root',
+      password:'',
+    database : 'baidunews'
+});
+/* GET home page. */
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
+router.post('/selectClass',function(req,res,next){
+  var nclass=req.body.nclass;
+  var str="'"+nclass+"'";
+  conn.query('select * from news where nclass='+str+ 'order by nid desc',function(err,rows){
+    if(err){
+      throw err;
+    }else{
+      res.send(rows);
+    }
+  });
+});
+router.post('/selectLi',function(req,res,next){
+  var nclass=req.body.nclass;
+  var str="'"+nclass+"'";
+  conn.query('select * from news where nclass='+str + 'order by nid desc',function(err,rows){
+    if(err){
+      throw err;
+    }else{
+      res.send(rows);
+      console.log(rows);
+    }
+  });
+});
+module.exports = router;
